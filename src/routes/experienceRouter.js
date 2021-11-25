b12bc35a8a8cc657e94d4829944376ed535b96fb
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-11-25 13:48:54 -0300

    feat: Adicionado o esquelo do BD em create-and-populate.js

--- src/routes/experienceRouter.js
+++ src/routes/experienceRouter.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.get("/:id", ExperienceController.show);
 router.get("/", ExperienceController.index);
-// router.post("/", ExperienceController.save);
-// router.put("/:id", ExperienceController.update);
-// router.delete("/:id", ExperienceController.remove);
+router.post("/", ExperienceController.save);
+router.put("/:id", ExperienceController.update);
+router.delete("/:id", ExperienceController.remove);
 
 module.exports = router;
