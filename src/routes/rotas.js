4f6f5e5e5f752d804dcaf5f10f3ad960400198ca
Adriel Saul <adrielsaul@gmail.com>
2021-11-25 16:10:36 -0300

    FEAT: ROOM ROUTE

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const experienceRouter = require("./experienceRouter");
+const roomRouter = require("./roomRouter");
 
 
 //middleware
