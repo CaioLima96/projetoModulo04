1d7db9096225be80bf178790541552c61568e407
marianamachadox <marianalmachado09@gmail.com>
2021-11-25 23:35:40 -0300

    feat: adiciona paymentDao

---
+++ src/dao/paymentDao.js
@@ -0,0 +1,100 @@
+const bd = require('../infra/sqlite-db');
+const { EXPERIENCES_TABLE: TABLE} = require('../utils/appConfig')
+
+
+class PaymentDao {
+    constructor(dbConn) {
+        this.dbConn = dbConn
+    }
+
+    getPaymentById = (id) => {
+        return new Promise((resolve, reject) => {
+            this.dbConn.all(
+                `SELECT * FROM ${TABLE} WHERE id like ?`,
+                id,
+                (error, results) => {
+                    console.log("Usuário único retornado com sucesso")
+                    if(error) {
+                        reject("Error: " + error)
+                    } else {
+                        resolve(results)
+                    }
+                }
+            )
+        })
+    }
+
+    getAllPayment = () => {
+        return new Promise((resolve, reject) => {
+            this.dbConn.all(
+                `SELECT * FROM ${TABLE}`,
+                (error, results) => {
+                    console.log("Todos os Usuários retornados com sucesso")
+                    if(error) {
+                        reject("Error: " + error)
+                    } else {
+                        resolve(results)
+                    }
+                }
+            )
+        })
+    }
+
+    saveUser = (exp) => {
+        return new Promise((resolve, reject) => {
+          this.dbConn.run(
+            `INSERT INTO ${TABLE} VALUES (?, ?, ?, ?)`,
+            exp.id,
+            exp.idUser,
+            exp.idStaff,
+            exp.valorTotal,
+            (error) => {
+                console.log("Rota post feita com sucesso")
+              if (error) {
+                reject("Error: " + error);
+              } else {
+                resolve(true);
+              }
+            }
+          );
+        });
+    }
+
+    deletePayment = (id) => {
+        return new Promise((resolve, reject) => {
+          this.dbConn.run(`DELETE FROM ${TABLE} WHERE id = ?`, id, 
+          (error) => {
+            console.log("Rota delete feita com sucesso")
+            if (error) {
+              reject(error);
+            } else {
+              resolve(true);
+            }
+          })
+        })
+    }
+    
+    updatePayment = (id, exp) => {
+        return new Promise((resolve, reject) => {
+          this.dbConn.run(
+            `UPDATE ${TABLE} SET id = ?, idUser = ?, idStaff = ?, valorTotal  = ?, WHERE id = ? `, 
+            exp.id,
+            exp.idUser,
+            exp.idStaff,
+            exp.valorTotal,
+            id,
+            (error) => {
+                console.log("Rota update feita com sucesso")
+              if (error) {
+                reject(error);
+              } else {
+                resolve(true);
+              }
+            }
+          )
+        })
+    }
+}
+
+
+module.exports = new PaymentDao(bd)
