080110d60b3cafd45b7d70f1283dd8d350f467fc
marianamachadox <marianalmachado09@gmail.com>
2021-11-26 12:49:22 -0300

    fix: corrige linha 4 e ss do paymentModel

--- src/models/paymentModel.js
+++ src/models/paymentModel.js
@@ -1,12 +1,11 @@
 const { v4: uuid } = require("uuid");
 
 class PaymentModel {
-    constructor(forma_de_pagamento, bandeira_cartão, cupom_desconto, status) {
-        this.id = uuid(),
-        this.forma_de_pagamento = forma_de_pagamento,
-        this.bandeira_cartão = bandeira_cartão,
-        this.cupom_desconto = cupom_desconto,
-        this.status = status
+    constructor(id, idUser, idStaff, valorTotal) {
+            this.id = uuid(),
+            this.idUser = idUser,
+            this.idStaff = idStaff,
+            this.valorTotal = valorTotal
     }
 }
 
