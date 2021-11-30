8f6cf2ca97dfa7dcf8335ce2c0405774467dddda
LOrena Zanferrari <lorena.zanferrari@msn.com>
2021-11-30 14:19:36 -0300

    Feat: add entidade de endereço

---
+++ src/models/addressModel.js
@@ -0,0 +1,18 @@
+const { v4: uuid } = require("uuid");
+
+class AddressModel {
+    constructor(cep, logradouro, numero, complemento, bairro, cidade, estado, pais) {
+        this.id = uuid(),
+        this.cep = cep,
+        this.logradouro = logradouro,
+        this.numero = numero,
+        this.complemento = complemento,
+        this.bairro = bairro,
+        this.cidade = cidade,
+        this.estado = estado,
+        this.pais = pais,
+        this.id_user = id_user
+    }
+}
+
+module.exports = AddressModel
