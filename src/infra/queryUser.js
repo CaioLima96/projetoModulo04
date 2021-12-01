190241b476b361663ee4bfdb8ffc44836b819c67
Leo Silva <leonardo.ghost@gmail.com>
2021-12-01 01:12:43 -0300

    FEAT: adicionando uuid

--- src/infra/queryUser.js
+++ src/infra/queryUser.js
@@ -4,27 +4,27 @@ CREATE TABLE IF NOT EXISTS "users" (
     "id" CHAR(36) PRIMARY KEY,
     "nome" varchar(100),
     "email" VARCHAR(80),
-    "senha" VARCHAR(50),
+    "senha" CHAR(50),
     "CPF" INTEGER(11) NOT NULL,
-    "id_adress" VARCHAR(36),
+    "id_adress" CHAR(36),
     FOREIGN KEY(id_endereco) REFERENCES adress(id)
   );
 `
 
-user_id_1 = uuid();
-user_id_2 = uuid();
-user_id_3 = uuid();
+let user_id_1 = uuid();
+let user_id_2 = uuid();
+let user_id_3 = uuid();
 
-user_senha_1 = sha256((Math.random() + 1).toString(36).substring(2));
-user_senha_2 = sha256((Math.random() + 1).toString(36).substring(2));
-user_senha_3 = sha256((Math.random() + 1).toString(36).substring(2));
+let user_senha_1 = sha256((Math.random() + 1).toString(36). substring(2));
+let user_senha_2 = sha256((Math.random() + 1).toString(36).substring(2));
+let user_senha_3 = sha256((Math.random() + 1).toString(36).substring(2));
 
-user_adress_id_1 = 123456;
-user_adress_id_2 = 789456;
-user_adress_id_3 = 456123;
+let user_adress_id_1 = uuid();
+let user_adress_id_2 = uuid();
+let user_adress_id_3 = uuid();
 
 const ADD_USERS_DATA = `
-INSERT INTO users (id, nome, email, senha, CPF, id_endereco)
+INSERT INTO users (id, nome, email, senha, CPF, id_andress)
 VALUES 
     ('${user_id_1}', 'Maria Silva', 'mariasilva@gmail.com', '${user_senha_1}', '48018705038','${user_adress_id_1}')
     ('${user_id_2}', 'Lívia Caroline Raquel Sales', 'liviacarolineraquelsales_@metalplasma.com.br', '${user_senha_2}', '02214968822','${user_adress_id_2}')
