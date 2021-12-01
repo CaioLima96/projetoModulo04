cfe2377061c36c0dedbd3be8948c37344d36bd8c
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-12-01 11:37:21 -0300

    FEAT: adicionado os dados da tabela experience em create-and-populate

--- src/infra/create-and-populate.js
+++ src/infra/create-and-populate.js
@@ -34,13 +34,13 @@ const ADD_STAFF_DATA = ``
 
 function criaTabelaStaff() {
     db.run(STAFF_SCHEMA, (error)=> {
-       if (error) console.log("Erro ao criar tabela de usuários");
+       if (error) console.log("Erro ao criar tabela de staff");
     });
 }
 
 function populaTabelaStaff() {
     db.run(ADD_STAFF_DATA, (error)=> {
-       if (error) console.log("Erro ao popular tabela de usuários");
+       if (error) console.log("Erro ao popular tabela de staff");
     });
 }
 
@@ -70,7 +70,7 @@ const ADD_ROOM_DATA = `
     VALUES 
         ('${room_id_1}', 'casal simples', 010, 02, 01, 'livre', 300.00),
         ('${room_id_2}', 'triplo', 022, 03, 02, 'Ocupado', 400.00),
-        ('${room_id_3}', 'duplo', 014, 01, 'Ocupado', 300.00),
+        ('${room_id_3}', 'duplo', 014, 02, 01, 'Ocupado', 300.00),
         ('${room_id_4}', 'casal luxo', 101, 02, 10, 'Livre', 800.00)
 `
 
@@ -119,8 +119,7 @@ const ADD_EVENT_DATA = `
 
         ('${event_id_3}', 'Eventos corporativos', '2022-03-14 15:00:00', 2022-03-18 20:00:00', 400, 200.00, '18+', 'Atividades como convenções, treinamentos, reuniões e kick-off.', '4 dias', 'Salão'),
 
-        ('${event_id_4}', 'Cursos e workshops', '2022-03-20 10:00:00', '2022-04-05 16:00:00', 400, 150.00, '14+', 'Dursos e workshops de gastronomia com chefs renomados', '17 dias', 'Salão' )
-        
+        ('${event_id_4}', 'Cursos e workshops', '2022-03-20 10:00:00', '2022-04-05 16:00:00', 400, 150.00, '14+', 'Dursos e workshops de gastronomia com chefs renomados', '17 dias', 'Salão')
 `
 
 function criaTabelaEvent() {
@@ -139,19 +138,43 @@ function populaTabelaEvent() {
 
 //===== EXPERIENCES
 
-const EXPERIENCES_SCHEMA = ``
+const EXPERIENCES_SCHEMA = `
+    CREATE TABLE IF NOT EXISTS "experiences" (
+        "id" CHAR(36) PRIMARY KEY,
+        "nome" VARCHAR(150),
+        "valor_exp" DECIMAL(6,2),
+        "horario" DATETIME,
+        "duracao" VARCHAR(15),
+        "local_experience" VARCHAR(15),
+        "dia_semana" VARCHAR(50),
+        "qtd_pessoas" INTEGER(5),
+        "descricao" VARCHAR(500)
+    );
+`
 
-const ADD_EXPERIENCES_DATA = ``
+exp_id_1 = uuid()
+exp_id_2 = uuid()
+exp_id_3 = uuid()
+
+const ADD_EXPERIENCES_DATA = `
+    INSERT INTO experiences (id, nome, valor_exp, horario, duracao, local_experience, dia_semana, qtd_pessoas, descricao)
+    VALUES 
+        ('${exp_id_1}', 'Massagem Facial', 300.00, '10:00:00', '1h20min', 'SPA', 'fds', 02, 'Deliciosa e completa massagem corporal relaxante. Acompanha alongamento na região do pescoço promovendo uma melhora nas tensões. Um momento de bem-estar dos pés à cabeça.'),
+
+        ('${exp_id_2}', 'Jantar à luz de vela', 2500.00, '20:00:00', '1:30:00', 'Jardim do hotel', 'sexta, sabado, domingo', 02, 'Não há nada melhor do que celebrar um sentimento tão lindo como o amor! O Jantar à luz de velas é servido em espaço exclusivo no jardim do hotel – um ambiente com flores, árvores e parreiras!'),
+
+        ('${exp_id_3}', 'Chá da tarde', 90.00, '17:00:00', '1:00:00', 'segunda à sexta', 05, 'O maravilhoso Chá das Cinco serve sabor e elegância em uma agradável experiência. Na mesa posta, doçuras e ternuras criam memórias afetivas durante um delicioso momento de partilha.')
+`
 
 function criaTabelaExperience() {
     db.run(EXPERIENCES_SCHEMA, (error)=> {
-       if (error) console.log("Erro ao criar tabela de usuários");
+       if (error) console.log("Erro ao criar tabela de experiência");
     });
 }
 
 function populaTabelaExperience() {
     db.run(ADD_EXPERIENCES_DATA, (error)=> {
-       if (error) console.log("Erro ao popular tabela de usuários");
+       if (error) console.log("Erro ao popular tabela de experiência");
     });
 }
 
@@ -165,13 +188,13 @@ const  ADD_BOOKING_DATA = ``
 
 function criaTabelaBooking() {
     db.run(BOOKING_SCHEMA, (error)=> {
-       if (error) console.log("Erro ao criar tabela de usuários");
+       if (error) console.log("Erro ao criar tabela de booking");
     });
 }
 
 function populaTabelaBooking() {
     db.run(ADD_BOOKING_DATA, (error)=> {
-       if (error) console.log("Erro ao popular tabela de usuários");
+       if (error) console.log("Erro ao popular tabela de booking");
     });
 }
 
@@ -185,13 +208,13 @@ const ADD_PAYMENT_DATA = ``
 
 function criaTabelaPayment() {
     db.run(ADD_PAYMENT_DATA, (error)=> {
-       if (error) console.log("Erro ao criar tabela de usuários");
+       if (error) console.log("Erro ao criar tabela de payment");
     });
 }
 
 function populaTabelaPayment() {
     db.run(PAYMENT_SCHEMA, (error)=> {
-       if (error) console.log("Erro ao popular tabela de usuários");
+       if (error) console.log("Erro ao popular tabela de payment");
     });
 }
 
@@ -201,10 +224,6 @@ function populaTabelaPayment() {
 db.serialize( ()=> {
     criaTabelaUser()
     populaTabelaUser()
-    criaTabelaBooking()
-    populaTabelaBooking()
-    criaTabelaPayment()
-    populaTabelaPayment()
     criaTabelaStaff()
     populaTabelaStaff()
     criaTabelaRoom()
@@ -213,4 +232,8 @@ db.serialize( ()=> {
     populaTabelaEvent()
     criaTabelaExperience()
     populaTabelaExperience()
+    criaTabelaBooking()
+    populaTabelaBooking()
+    criaTabelaPayment()
+    populaTabelaPayment()
 });
--- src/models/experienceModel.js
+++ src/models/experienceModel.js
@@ -1,14 +1,13 @@
 const { v4: uuid } = require("uuid");
 
 class ExperienceModel {
-    constructor(nome, valor_exp, id_booking, horario, duracao, local, dia_semana, qtd_pessoas, descricao) {
+    constructor(nome, valor_exp, horario, duracao, local_experience, dia_semana, qtd_pessoas, descricao) {
         this.id = uuid(),
         this.nome = nome,
         this.valor_exp = valor_exp,
-        this.id_booking = id_booking,
         this.horario = horario,
         this.duracao = duracao,
-        this.local = local,
+        this.local_experience = local_experience,
         this.dia_semana = dia_semana,
         this.qtd_pessoas = qtd_pessoas
         this.descricao = descricao
