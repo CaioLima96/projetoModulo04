147fd1ea7d4a94c2894d9a9f779381a881fce77e
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-12-03 10:47:37 -0300

    FIX: concertado erro na lista da rota get

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -14,41 +14,52 @@ const addressRouter = require("./addressRouter");
 
 // ROTAS
 
+router.use("/experience", experienceRouter);//
+router.use("/room", roomRouter);//
+router.use("/payment", paymentRouter);//
+router.use("/event", eventRouter);//
+router.use("/user", userRouter);//
+router.use("/staff", staffRouter);//
+router.use("/booking", bookingRouter);
+router.use("/address", addressRouter);//
+
 router.get("/", (req, res) => {
     res.send(
     `
-        <h1>Bem vindo(a) à Api Hotel Resilia Palace!</h1>
-        <ul>
-            <li style="font-size: 1.25rem;">Para ver os usuários, basta por /user após o fim da url.</li>
+        <div style="padding-left: 0.938rem; padding-right: 0.938rem;">
+            <h1>Bem vindo(a) à Api Hotel Resilia Palace!</h1>
+            <ul>
+
+                <li  style="font-size: 1.25rem;">Para ver os usuários, basta por /user após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os endereços, basta por /address após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os quartos, basta por /room após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os pagamentos, basta por /payment após o fim da url</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os eventos, basta por /event após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver as experiências, basta por /experience após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os funcionários, basta por /staff após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver as reservas, basta por /booking após o fim da url.</li>
+
+            </ul>
+
             <br>
-            <li style="font-size: 1.25rem;">Para ver os endereços, basta por /address após o fim da url.</li>
 
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
+            <p><a href="https://github.com/CaioLima96/projetoModulo04" target="_blank">Repositório da api</a></p>
 
-        <br>
+            <br>
 
-        <p><a href="https://github.com/CaioLima96/projetoModulo04" target="_blank">Repositório da api</a></p>
-
-        <br>
-
-        <p>Api criada por: Grupo 1 - Resilia</p
+            <p>Api criada por: Grupo 1 - Resilia</p>
+        </div>
 
     `
     );
 });
-
-router.use("/experience", experienceRouter);
-router.use("/room", roomRouter);
-router.use("/payment", paymentRouter);
-router.use("/event", eventRouter);
-router.use("/user", userRouter);
-router.use("/staff", staffRouter);
-router.use("/booking", bookingRouter);
-router.use("/address", addressRouter);
-
 
 
 module.exports = router;
--- src/utils/appConfig.js
+++ src/utils/appConfig.js
@@ -4,7 +4,7 @@ module.exports = AppConfiguration = {
     USERS_TABLE: 'USERS',
     BOOKING_TABLE: 'BOOKING',
     PAYMENT_TABLE: 'PAYMENT',
-    STAFF_TABLE: 'STAFF',
+    STAFF_TABLE: 'staff',
     ROOM_TABLE: 'ROOMS',
     EVENT_TABLE: 'EVENTS',
     EXPERIENCES_TABLE: 'EXPERIENCES',
