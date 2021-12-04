8431bf99250b8f82feeb95dbed4005f730776d8f
Leo Silva <leonardo.ghost@gmail.com>
2021-12-03 22:08:26 -0300

    TEST: Criacao código de teste

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 //"puxa" o express
 const express = require('express');
 const app = express();
-const cors = require('cors')
 
 //Porta que o app vai ser rodado
 const {PORT} = require('./utils/appConfig')
@@ -12,7 +11,6 @@ app.use(express.json())
 app.use((req, res, next) => {
     next()
 })
-
 
 //puxa as rotas CRUD
 const router = require('./routes/rotas')
---
+++ src/test/Router.test.js
@@ -0,0 +1,72 @@
+const request = require('supertest')
+const app = require('../app')
+
+//TESTES //nome,email,senha,cpf,id_address
+
+describe('POST Route', () => {
+    it('Criando um usuário para poder testar', async () => {
+        const res = await request(app)
+            .post('/user')
+            .send({
+                "id": 1,
+                "nome": "Batman",
+                "email": "IamBatman@heromail.com",
+                "senha": "TananananBatman456",
+                "cpf": 14725835711,
+                "id_address": "12"
+            })
+        expect(res.statusCode).toEqual(201)
+        expect(response.body).toHaveProperty('id','nome','email','senha','CPF','id_address')
+    })
+})
+
+// describe(' Teste método GET', () => {
+//     it('should return some portfolios', async () => {
+//         const res = await request(app)
+//             .get('/')
+//         expect(res.statusCode).toEqual(200)
+//         expect(res.body).toHaveProperty('result')
+//     })
+//     it('should return the portfolio with id=1', async () => {
+//         const res = await request(app)
+//             .get('/portfolio/1')
+//         expect(res.statusCode).toEqual(200)
+//         expect(res.body).toHaveProperty('result')
+//         expect(res.body.result.id).toEqual(1)
+//     })
+//     it('should return the portfolio with tag=cartoon', async () => {
+//         const res = await request(app)
+//             .get('/portfolio/tag/Cartoon')
+//         expect(res.statusCode).toEqual(200)
+//         expect(res.body).toHaveProperty('result')
+//         expect(res.body.result.tag).toEqual("cartoon")
+//     })
+// })
+
+// describe('PUT Route', () => {
+//     it('should return error=false after update a portfolio', async () => {
+//         try {
+//             const res = await request(app)
+//                 .put('/portfolio/3')
+//                 .send({
+//                     "avaliable": false
+//                 })
+//             expect(res.statusCode).toEqual(200)
+//             expect(res.body.error).toEqual(false)
+//         } catch (e) {
+//             console.log(e.message)
+//         }
+
+//     })
+// })
+// describe('DELETE Route', () => {
+//     it('should return error=false after delete a portfolio', async () => {
+
+//         const res = await request(app)
+//             .delete('/portfolio/2')
+//         expect(res.statusCode).toEqual(200)
+//         expect(res.body.error).toEqual(false)
+
+//     })
+// })
+
